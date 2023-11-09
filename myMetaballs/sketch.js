f915f2eb8e2d0e50234a0f5946b278cc332fea8b
CODASx0 <codas0327@gmail.com>
2023-11-09 16:23:14 +0800

    升级

--- myMetaballs/sketch.js
+++ myMetaballs/sketch.js
@@ -1,12 +1,15 @@
 //设置精度
-let dpr = 15;
+let dpr = 10;
 //画布上出现的第一类圆型
 let circle0 = [];
 //初始化几个后面会用到的参数
 let xNum = 0;
 let yNum = 0;
-//屏幕上显示的点阵
-let pointVolume = [];
+
+//设置阈值（失效，待研究）
+let lim = 1;
+//定义画布
+let canvas;
 
 //定义类：画在上面的圆
 class circleDraw {
@@ -23,40 +26,135 @@ function drawSketchPoint(W, H) {
 	for (let i = 0; i < W + 1; i++) {
 		for (let j = 0; j < H + 1; j++) {
 			//储存到圆心距离的倒数
-			pointVolume[i + j * W] = circle0[0].r / sqrt(sq(circle0[0].x - i * dpr) + sq(circle0[0].y - j * dpr));
+			inputValue[i][j] = circle0[0].r / sqrt(sq(circle0[0].x - i * dpr) + sq(circle0[0].y - j * dpr));
 			for (let n = 1; n < circle0.length; n++) {
-				pointVolume[i + j * W] += circle0[n].r / sqrt(sq(circle0[n].x - i * dpr) + sq(circle0[n].y - j * dpr));
+				inputValue[i][j] += circle0[n].r / sqrt(sq(circle0[n].x - i * dpr) + sq(circle0[n].y - j * dpr));
 			}
 		}
 	}
 
+
+
 	for (let i = 0; i < W; i++) {
 		for (let j = 0; j < H; j++) {
 
+			var nw = inputValue[i][j];
+			var ne = inputValue[i + 1][j];
+			var se = inputValue[i + 1][j + 1];
+			var sw = inputValue[i][j + 1];
+			var a = [i * dpr + dpr * myLerp(1, nw, ne), j * dpr];
+			var b = [
+				i * dpr + dpr,
+				j * dpr + dpr * myLerp(1, ne, se)
+			];
+			var c = [
+				i * dpr + dpr * myLerp(1, sw, se),
+				j * dpr + dpr
+			];
+			var d = [i * dpr, j * dpr + dpr * myLerp(1, nw, sw)];
+
+			/*noStroke();
+			fill(inputValue[i][j] * 100);
+			ellipse(i * dpr, j * dpr, dpr, dpr);*/
+
+			gridValue[i][j] = binaryToType(
+				inputValue[i][j] > lim,
+				inputValue[i + 1][j] > lim,
+				inputValue[i + 1][j + 1] > lim,
+				inputValue[i][j + 1] > lim
+			)
+
+			stroke(255);
+			switch (gridValue[i][j]) {
+				case 1:
+				case 14:
+					line(d[0], d[1], c[0], c[1]);
+					break;
+
+				case 2:
+				case 13:
+					line(b[0], b[1], c[0], c[1]);
+					break;
+
+				case 3:
+				case 12:
+					line(d[0], d[1], b[0], b[1]);
+					break;
+
+				case 11:
+				case 4:
+					line(a[0], a[1], b[0], b[1]);
+					break;
+
+				case 5:
+					line(d[0], d[1], a[0], a[1]);
+					line(c[0], c[1], b[0], b[1]);
+					break;
+				case 6:
+				case 9:
+					line(c[0], c[1], a[0], a[1]);
+					break;
+
+				case 7:
+				case 8:
+					line(d[0], d[1], a[0], a[1]);
+					break;
+
+				case 10:
+					line(a[0], a[1], b[0], b[1]);
+					line(c[0], c[1], d[0], d[1]);
+					break;
+				default:
+					break;
+			}
 		}
 	}
 
 }
 
+function myLerp(x, x0, x1, y0 = 0, y1 = 1) {
+	if (x0 === x1) {
+		return null;
+	}
+
+	return y0 + ((y1 - y0) * (x - x0)) / (x1 - x0);
+}
+
+function binaryToType(nw, ne, se, sw) {
+	a = [nw, ne, se, sw];
+	return a.reduce((res, x) => (res << 1) | x);
+}
+
 function setup() {
-	createCanvas(1600, 800);
+	canvas = createCanvas(windowWidth, windowHeight);
 	xNum = width / dpr | 0;
 	yNum = height / dpr | 0;
 
-	circle0[1] = new circleDraw(300, 200, 100);
-	circle0[2] = new circleDraw(600, 200, 100);
+	//初始化二维数组
+	inputValue = new Array(xNum + 1);
+	gridValue = new Array(xNum + 1);
+	for (let i = 0; i <= xNum; i++) {
+		inputValue[i] = new Array(yNum + 1);
+		gridValue[i] = new Array(yNum + 1);
+	}
+
+
+	circle0[1] = new circleDraw(width * 0.5, height * 0.5, 50);
+	/*circle0[2] = new circleDraw(600, 200, 100);
 	circle0[3] = new circleDraw(900, 200, 100);
 	circle0[4] = new circleDraw(1200, 200, 100);
 	circle0[5] = new circleDraw(900, 400, 100);
-	circle0[6] = new circleDraw(1200, 400, 100);
+	circle0[6] = new circleDraw(1200, 400, 100);*/
 
 }
 
 function draw() {
 	background(0, 0, 0);
-	//点阵初始化
+
 	circle0[0] = new circleDraw(mouseX, mouseY, 100);
-	pointVolume = []
+
 	drawSketchPoint(xNum, yNum);
 
 }
+
+function windowResized() { resizeCanvas(windowWidth, windowHeight); }
