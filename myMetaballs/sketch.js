c5b843c122efc633e9997ff21de4f0e00ba646e5
CODASx0 <codas0327@gmail.com>
2023-11-12 21:23:01 +0800

    大更新

--- myMetaballs/sketch.js
+++ myMetaballs/sketch.js
@@ -1,19 +1,21 @@
 //设置精度
-let dpr = 10;
-//画布上出现的第一类圆型
-let circle0 = [];
-
-
-//let fillCol0 = color('rgb(0,0,200)');
-//let strokeCol0 = color('rgb(0,0,20)');
-
-
-//初始化几个后面会用到的参数
-let xNum = 0;
-let yNum = 0;
+let dpr = 8;
 
 //设置全局阈值，相当于统一调整半径，非必要不调整
-let lim = 1;
+let lim = 2;
+
+
+//初始化画布的宽、高、图层数量
+let xNum = 0;
+let yNum = 0;
+let zNum = 1;
+
+//初始化不同图层上的圆
+circleArray = new Array(zNum);
+for (z = 0; z < zNum; z++) {
+	circleArray[z] = new Array();
+}
+
 //定义画布
 let canvas;
 
@@ -27,97 +29,207 @@ class circleDraw {
 }
 
 //把点阵上的数值计算后视觉化呈现出来
-function drawSketchPoint(W, H) {
-	//把第一类圆形的数值叠加后计算出来
-	for (let i = 0; i < W + 1; i++) {
-		for (let j = 0; j < H + 1; j++) {
-			//储存到圆心距离的倒数
-			inputValue[i][j] = circle0[0].r / sqrt(sq(circle0[0].x - i * dpr) + sq(circle0[0].y - j * dpr));
-			for (let n = 1; n < circle0.length; n++) {
-				inputValue[i][j] += circle0[n].r / sqrt(sq(circle0[n].x - i * dpr) + sq(circle0[n].y - j * dpr));
+function drawSketchPoint(W, H, Z) {
+	for (let z = 0; z < Z; z++) {
+
+		//把第 Z 类圆形的数值叠加后计算出来
+		for (let i = 0; i < W + 1; i++) {
+			for (let j = 0; j < H + 1; j++) {
+				//储存到圆心距离的倒数
+				inputValue[i][j][z] = circleArray[z][0].r / sqrt(sq(circleArray[z][0].x - i * dpr) + sq(circleArray[z][0].y - j * dpr));
+				for (let n = 1; n < circleArray[z].length; n++) {
+					inputValue[i][j][z] += circleArray[z][n].r / sqrt(sq(circleArray[z][n].x - i * dpr) + sq(circleArray[z][n].y - j * dpr));
+				}
 			}
 		}
-	}
 
 
 
-	for (let i = 0; i < W; i++) {
-		for (let j = 0; j < H; j++) {
+		for (let i = 0; i < W; i++) {
+			for (let j = 0; j < H; j++) {
+				var nw = inputValue[i][j][z];
+				var ne = inputValue[i + 1][j][z];
+				var se = inputValue[i + 1][j + 1][z];
+				var sw = inputValue[i][j + 1][z];
+				var a = [i * dpr + dpr * myLerp(lim, nw, ne), j * dpr];
+				var b = [
+					i * dpr + dpr,
+					j * dpr + dpr * myLerp(lim, ne, se)
+				];
+				var c = [
+					i * dpr + dpr * myLerp(lim, sw, se),
+					j * dpr + dpr
+				];
+				var d = [i * dpr, j * dpr + dpr * myLerp(lim, nw, sw)];
 
-			var nw = inputValue[i][j];
-			var ne = inputValue[i + 1][j];
-			var se = inputValue[i + 1][j + 1];
-			var sw = inputValue[i][j + 1];
-			var a = [i * dpr + dpr * myLerp(lim, nw, ne), j * dpr];
-			var b = [
-				i * dpr + dpr,
-				j * dpr + dpr * myLerp(lim, ne, se)
-			];
-			var c = [
-				i * dpr + dpr * myLerp(lim, sw, se),
-				j * dpr + dpr
-			];
-			var d = [i * dpr, j * dpr + dpr * myLerp(lim, nw, sw)];
+				gridValue[i][j][z] = binaryToType(
+					inputValue[i][j][z] > lim,
+					inputValue[i + 1][j][z] > lim,
+					inputValue[i + 1][j + 1][z] > lim,
+					inputValue[i][j + 1][z] > lim
+				)
 
-			/*noStroke();
-			fill(inputValue[i][j] * 100);
-			ellipse(i * dpr, j * dpr, dpr, dpr);*/
 
-			gridValue[i][j] = binaryToType(
-				inputValue[i][j] > lim,
-				inputValue[i + 1][j] > lim,
-				inputValue[i + 1][j + 1] > lim,
-				inputValue[i][j + 1] > lim
-			)
+				noStroke();
+				fill("#F0E7E4");
+				//绘制 Metaballs
+				switch (gridValue[i][j][z]) {
+					case 1:
+					case 14:
+						//line(d[0], d[1], c[0], c[1]);
+						if (inputValue[i][j + 1][z] > lim) {
+							triangle(d[0], d[1], c[0], c[1], d[0], c[1]);
+						} else {
 
-			//gridValue[i][j] 
+							beginShape();
+							vertex(d[0], d[1]);
+							vertex(c[0], c[1]);
+							vertex(b[0], c[1]);
+							vertex(b[0], a[1]);
+							vertex(d[0], a[1]);
+							endShape(CLOSE);
+						}
+						break;
+					case 2:
+					case 13:
+						//line(b[0], b[1], c[0], c[1]);
+						if (inputValue[i + 1][j + 1][z] > lim) {
+							triangle(b[0], b[1], c[0], c[1], b[0], c[1])
+						} else {
+							beginShape();
+							vertex(b[0], b[1]);
+							vertex(c[0], c[1]);
+							vertex(d[0], c[1]);
+							vertex(d[0], a[1]);
+							vertex(b[0], a[1]);
+							endShape(CLOSE);
+						}
+						break;
 
-			stroke(200);
-			switch (gridValue[i][j]) {
-				case 1:
-				case 14:
-					//noStroke();
-					//fill(fillCol0);
-					//triangle(d[0], d[1], c[0], c[1], d[0], c[1]);
-					//stroke(strokeCol0);
-					line(d[0], d[1], c[0], c[1]);
-					break;
+					case 3:
+					case 12:
+						//line(d[0], d[1], b[0], b[1]);
+						if (inputValue[i][j][z] > lim) {
+							beginShape();
+							vertex(d[0], a[1]);
+							vertex(b[0], a[1]);
+							vertex(b[0], b[1]);
+							vertex(d[0], d[1]);
+							endShape(CLOSE);
+						} else {
+							beginShape();
+							vertex(d[0], c[1]);
+							vertex(b[0], c[1]);
+							vertex(b[0], b[1]);
+							vertex(d[0], d[1]);
+							endShape(CLOSE);
+						}
+						break;
 
-				case 2:
-				case 13:
-					line(b[0], b[1], c[0], c[1]);
-					break;
+					case 11:
+					case 4:
+						//line(a[0], a[1], b[0], b[1]);
+						if (inputValue[i + 1][j][z] > lim) {
+							triangle(a[0], a[1], b[0], b[1], b[0], a[1]);
+						} else {
+							beginShape();
+							vertex(a[0], a[1]);
+							vertex(b[0], b[1]);
+							vertex(b[0], c[1]);
+							vertex(d[0], c[1]);
+							vertex(d[0], a[1]);
+							endShape(CLOSE);
+						}
+						break;
 
-				case 3:
-				case 12:
-					line(d[0], d[1], b[0], b[1]);
-					break;
+					case 5:
+						//line(d[0], d[1], a[0], a[1]);
+						//line(c[0], c[1], b[0], b[1]);
+						if (inputValue[i][j][z] > lim) {
+							beginShape(TRIANGLES);
+							vertex(d[0], a[1]);
+							vertex(a[0], a[1]);
+							vertex(d[0], d[1]);
+							vertex(b[0], b[1]);
+							vertex(c[0], c[1]);
+							vertex(b[0], c[1]);
+							endShape();
+						} else {
+							beginShape();
+							vertex(d[0], d[1]);
+							vertex(a[0], a[1]);
+							vertex(b[0], a[1]);
+							vertex(b[0], b[1]);
+							vertex(c[0], c[1]);
+							vertex(d[0], c[1]);
+							endShape(CLOSE);
+						}
+						break;
+					case 6:
+					case 9:
+						//line(c[0], c[1], a[0], a[1]);
+						if (inputValue[i][j][z] > lim) {
+							beginShape();
+							vertex(d[0], a[1]);
+							vertex(a[0], a[1]);
+							vertex(c[0], c[1]);
+							vertex(d[0], c[1]);
+							endShape(CLOSE);
+						} else {
+							beginShape();
+							vertex(b[0], a[1]);
+							vertex(a[0], a[1]);
+							vertex(c[0], c[1]);
+							vertex(b[0], c[1]);
+							endShape(CLOSE);
+						}
+						break;
 
-				case 11:
-				case 4:
-					line(a[0], a[1], b[0], b[1]);
-					break;
+					case 7:
+					case 8:
+						//line(d[0], d[1], a[0], a[1]);
+						if (inputValue[i][j][z] > lim) {
+							triangle(d[0], a[1], a[0], a[1], d[0], d[1]);
+						} else {
+							beginShape();
+							vertex(d[0], d[1]);
+							vertex(a[0], a[1]);
+							vertex(b[0], a[1]);
+							vertex(b[0], c[1]);
+							vertex(d[0], c[1]);
+							endShape(CLOSE);
+						}
+						break;
 
-				case 5:
-					line(d[0], d[1], a[0], a[1]);
-					line(c[0], c[1], b[0], b[1]);
-					break;
-				case 6:
-				case 9:
-					line(c[0], c[1], a[0], a[1]);
-					break;
+					case 10:
+						//line(a[0], a[1], b[0], b[1]);
+						//line(c[0], c[1], d[0], d[1]);
+						if (inputValue[i][j][z] > lim) {
+							beginShape();
+							vertex(d[0], a[1]);
+							vertex(a[0], a[1]);
+							vertex(b[0], b[1]);
+							vertex(b[0], c[1]);
+							vertex(c[0], c[1]);
+							vertex(d[0], d[1]);
+							endShape(CLOSE);
+						} else {
+							beginShape(TRIANGLES);
+							vertex(a[0], a[1]);
+							vertex(b[0], b[1]);
+							vertex(b[0], a[1]);
+							vertex(d[0], c[1]);
+							vertex(d[0], c[1]);
+							endShape();
+						}
+						break;
+					case 15:
+						rect(d[0], a[1], dpr, dpr);
 
-				case 7:
-				case 8:
-					line(d[0], d[1], a[0], a[1]);
-					break;
+					default:
+						break;
+				}
 
-				case 10:
-					line(a[0], a[1], b[0], b[1]);
-					line(c[0], c[1], d[0], d[1]);
-					break;
-				default:
-					break;
 			}
 		}
 	}
@@ -148,19 +260,24 @@ function draw() {
 	//动态响应
 	xNum = width / dpr | 0;
 	yNum = height / dpr | 0;
+	zNum = 1;
 	//动态初始化二维数组
 	inputValue = new Array(xNum + 1);
 	gridValue = new Array(xNum + 1);
 	for (let i = 0; i <= xNum; i++) {
 		inputValue[i] = new Array(yNum + 1);
 		gridValue[i] = new Array(yNum + 1);
+		for (let j = 0; j <= yNum; j++) {
+			inputValue[i][j] = new Array(zNum);
+			gridValue[i][j] = new Array(zNum);
+		}
 	}
-	background(0, 0, 0);
+	background(255);
 
-	circle0[1] = new circleDraw(width * 0.5, height * 0.5, 60);
-	circle0[0] = new circleDraw(mouseX, mouseY, 60);
+	circleArray[0][1] = new circleDraw(width * 0.5, height * 0.5, 60);
+	circleArray[0][0] = new circleDraw(mouseX, mouseY, 60);
 
-	drawSketchPoint(xNum, yNum);
+	drawSketchPoint(xNum, yNum, zNum);
 
 }
 
