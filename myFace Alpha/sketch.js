7cb8b7ea29c1c01e730c2d59fe6d453a57a00d31
CODASx0 <jinkuang0327@live.com>
2024-01-16 14:06:29 +0800

    Updated

--- myFace Alpha/sketch.js
+++ myFace Alpha/sketch.js
@@ -4,6 +4,10 @@ let lips = [];
 let smoothingFactor = 0.6; // 平滑因子，取值范围是 0 到 1。值越大，平滑效果越强。
 let smoothedDetections = null;
 let returnReady = false;
+
+
+let gridDisplay = true;
+
 function updateDetections(newDetections) {
     if (smoothedDetections == null) {
         // 如果 smoothedDetections 还没有初始化，就直接使用 newDetections
@@ -99,49 +103,8 @@ for (let i = 0; i < lipsNum; i++) {
     lipsArray[i] = new Array(timeSetup);
 }
 
-//按下i键开始录制，松开i键结束录制
-function keyPressed() {
-    if ((key === 'i' || key === 'I') && detections != undefined) {
-
-        lipsArray[lipsArray.length] = new Array(timeSetup);
-        lipsArray[lipsArray.length - 1][0] = new myLips(JSON.parse(JSON.stringify(smoothedDetections)), returnReady, frameCount);
-        isRecording = true;
-    }
-    if (key === 'Backspace') {
-        lipsArray.pop();
-    }
-
-    if (key === ',') {
-        lipsArray[lipsArray.length] = new Array(timeSetup);
-        lipsArray[lipsArray.length - 1][0] = new myText('，', returnReady, frameCount);
-    }
-    if (key === '.') {
-        lipsArray[lipsArray.length] = new Array(timeSetup);
-        lipsArray[lipsArray.length - 1][0] = new myText('。', returnReady, frameCount);
-    }
-    if (key === '<') {
-        lipsArray[lipsArray.length] = new Array(timeSetup);
-        lipsArray[lipsArray.length - 1][0] = new myText('《', returnReady, frameCount);
-    }
-    if (key === '>') {
-        lipsArray[lipsArray.length] = new Array(timeSetup);
-        lipsArray[lipsArray.length - 1][0] = new myText('》', returnReady, frameCount);
-    }
-}
-
-//按下退格键删除最后一个字，按下回车键准备换行
-function keyReleased() {
-    if (key === 'i' || key === 'I') {
-        isRecording = false;
-        returnReady = false;
-    }
-
-    if (keyCode === RETURN) {
-        returnReady = true;
-    }
 
 
-}
 
 //录制函数
 function recordDetection() {
@@ -151,63 +114,163 @@ function recordDetection() {
 }
 
 //网格系统
-function showLips() {
+function showElement() {
+    //页边距
+    let pageMargin = 40;
+    //段间距
+    let paragraphSpacing = 40;
+    //段内边距
+    let padding = 10;
+    //行间距
+    let lineSpacing = 40;
+    //字间距
+    let wordSpacing = 40;
+
+    //字符占位大小
     let gridSize = 40;
-    let columns = canvas.width / gridSize;
-    let gridX = 0;
-    let gridY = 0;
-    let i = 0;
+
+    //计算每行字数
+    let columns = Math.floor((canvas.width - pageMargin * 2 - padding * 2) / (gridSize + wordSpacing));
+
+    //段落数
+    let paragraphNum = 0;
+    //统计每段字数
+    let paragraphLength = new Array(0);
+
+    //段落全宽行数
+    let paragraphFullLineNum = new Array(0);
+
+    //段落非全宽字数
+    let paragraphNotFullLine = new Array(0);
+
+    //圆角大小
+    let cornerRadius = 10;
+
+    //Y轴滚动值
+    let scrollY = 0;
+
+    //图案缩放系数
+    let ratio = 0.9;
+
+    //累计段落字数
+    for (let i = 0; i < lipsArray.length; i++) {
+        if (paragraphLength[paragraphNum] === undefined) {
+            paragraphLength[paragraphNum] = 0;
+        }
+        paragraphLength[paragraphNum] += 1;
 
 
+        if (lipsArray[i][0].isReturn || i === lipsArray.length - 1) {
 
-    while (i < lipsArray.length) {
+            paragraphFullLineNum[paragraphNum] = Math.floor(paragraphLength[paragraphNum] / columns);
+            paragraphNotFullLine[paragraphNum] = paragraphLength[paragraphNum] % columns;
+            paragraphNum++;
+        }
+    }
 
-        if (lipsArray[i][0].isReturn) {
-            gridX = 0;
-            gridY++;
+    for (let i = 0; i < paragraphNum; i++) {
+        let paragraphY = pageMargin + scrollY;
+        //根据前文段落情况计算当前段落的Y坐标
+        for (let j = 0; j < i; j++) {
+            //段间距
+            paragraphY += paragraphSpacing;
+            //段内边距
+            paragraphY += padding * 2;
+            //行间距(行数大于二时才有行间距)
+            paragraphY += lineSpacing * (paragraphFullLineNum[j] - 1);
+            //字符高度
+            paragraphY += gridSize * paragraphFullLineNum[j];
+            //非全宽行高度
+            //非全宽行高度
+            if (paragraphNotFullLine[j] > 0) {
+                console.log(j, paragraphNotFullLine[j]);
+                paragraphY += lineSpacing + gridSize;
+            }
         }
 
-        if (i === lipsArray.length - 1 && isRecording) {
-            fill(255, 0, 0, 255);
-            noStroke();
-        } else {
+        //计算段落衬底末端的X坐标
+        let paragraphEndX = pageMargin + padding * 2 + (gridSize + wordSpacing) * paragraphNotFullLine[i] - wordSpacing;
+
+        //绘制段落衬底
+        if (paragraphFullLineNum[i] === 0) {
             fill(255, 255);
             noStroke();
-        }
+            rect(pageMargin, paragraphY, paragraphEndX - pageMargin, gridSize + padding * 2, cornerRadius);
+        } else if (paragraphNotFullLine[i] === 0) {
+            //衬底最大宽度
+            let maxWidth = padding * 2 + (gridSize + wordSpacing) * columns - wordSpacing;
+            //全宽衬底高度
+            let fullHeight = padding * 2 + (gridSize + lineSpacing) * paragraphFullLineNum[i] - lineSpacing;
+            fill(255, 255);
+            noStroke();
+            rect(pageMargin, paragraphY, maxWidth, fullHeight, cornerRadius);
 
-        stroke(0, 20);
-        //定位十字线
-        let ratio = 0.6;
-
-        line(gridX * gridSize, gridY * gridSize, gridX * gridSize + gridSize, gridY * gridSize + gridSize);
-        line(gridX * gridSize + gridSize, gridY * gridSize, gridX * gridSize, gridY * gridSize + gridSize);
-
-        rect((gridX + 0.5 - ratio / 2) * gridSize, (gridY + 0.5 - ratio / 2) * gridSize, gridSize * ratio, gridSize * ratio);
-
-
-        let timeLine = (frameCount - lipsArray[i][0].time) % lipsArray[i].length;
-        lipsArray[i][timeLine].draw(gridX * gridSize, gridY * gridSize, gridSize, gridSize * ratio);
-
-        /*
-        for (let j = 0; j < lipsArray[i].length; j++) {
-            //使图形的透明度按照时间变化
-            let alpha = 4 * (1 - 4 * ((j + lipsArray[i].length - timeLine) % lipsArray[i].length )/ lipsArray[i].length);
-            if (alpha < 0) {alpha = 0;}
-            let num = j + lipsArray[i].length - timeLine;
-            //let alpha = 0;
-            if (j === timeLine) { 
-                alpha = 255;
-            }
-            lipsArray[i][j].draw(gridX * gridSize, gridY * gridSize, gridSize, gridSize * 0.6,255, alpha);
-        }*/
-        if (gridX < columns - 2) {
-            gridX++;
         } else {
-            gridX = 0;
-            gridY++;
+            //衬底最大宽度
+            let maxWidth = padding * 2 + (gridSize + wordSpacing) * columns - wordSpacing;
+            //全宽衬底高度
+            let fullHeight = padding * 2 + (gridSize + lineSpacing) * paragraphFullLineNum[i] - lineSpacing;
+            fill(255, 255);
+            noStroke();
+            rect(pageMargin, paragraphY, maxWidth, fullHeight, cornerRadius, cornerRadius, cornerRadius, 0);
+            //非全宽衬底
+            rect(pageMargin, paragraphY + fullHeight, paragraphEndX - pageMargin, gridSize + lineSpacing, 0, 0, cornerRadius, cornerRadius);
+
+        }
+    }
+
+    //累计字数
+    let wordNum = 0;
+    //分段落绘制图案(代码与上文段落衬底绘制相同)
+    for (i = 0; i < paragraphNum; i++) {
+
+        let paragraphY = pageMargin + scrollY;
+        //根据前文段落情况计算当前段落的Y坐标
+        for (let j = 0; j < i; j++) {
+            //段间距
+            paragraphY += paragraphSpacing;
+            //段内边距
+            paragraphY += padding * 2;
+            //行间距(行数大于二时才有行间距)
+            paragraphY += lineSpacing * (paragraphFullLineNum[j] - 1);
+            //字符高度
+            paragraphY += gridSize * paragraphFullLineNum[j];
+            //非全宽行高度
+            //非全宽行高度
+            if (paragraphNotFullLine[j] > 0) {
+                console.log(j, paragraphNotFullLine[j]);
+                paragraphY += lineSpacing + gridSize;
+            }
         }
 
-        i++;
+        for (j = 0; j < paragraphLength[i]; j++) {
+
+            //计算当前字在段落中的坐标
+            let posX = pageMargin + padding + (gridSize + wordSpacing) * (j % columns);
+            let posY = paragraphY + padding + (gridSize + lineSpacing) * Math.floor(j / columns);
+            //绘制当前字
+            stroke(0, 20);
+            if (gridDisplay) {
+                //定位辅助线
+                noFill();
+                line(posX, posY, posX + gridSize, posY + gridSize);
+                line(posX + gridSize, posY, posX, posY + gridSize);
+                rect(posX, posY, gridSize, gridSize)
+            }
+            //判断是否在录制中
+            if (wordNum === lipsArray.length - 1 && isRecording) {
+                fill(255, 0, 0, 255);
+                noStroke();
+            } else {
+                fill(255, 255);
+                noStroke();
+            }
+
+            let timeLine = (frameCount - lipsArray[wordNum][0].time) % lipsArray[wordNum].length;
+            lipsArray[wordNum][timeLine].draw(posX, posY, gridSize, gridSize * ratio);
+            wordNum++;
+        }
+
     }
 }
 
@@ -268,11 +331,10 @@ function draw() {
 
     fill(255, 255);
     noStroke();
-    showLips();
+    showElement();
 
     if (lipsArray.length > 0) {
         //console.log(lipsArray.length, lipsArray[lipsArray.length - 1].length, lipsArray[lipsArray.length - 1][0].isReturn);
-        console.log(lipsArray[0][0].left);
     }
 
     if (isRecording) {
@@ -283,4 +345,102 @@ function draw() {
 
     noStroke();
     ellipse(16, height - 16, 12, 12);
+}
+
+
+function keyPressed() {
+    if ((key === ' ') && detections != undefined) {
+        //按下空格键键开始录制
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myLips(JSON.parse(JSON.stringify(smoothedDetections)), returnReady, frameCount);
+        isRecording = true;
+    }
+    if (key === 'Backspace') {
+        lipsArray.pop();
+    }
+    //全键盘的字符映射
+    if (key === ',' || key === '，') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('，', returnReady, frameCount);
+    }
+    if (key === '.' || key === '。') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('。', returnReady, frameCount);
+    }
+    if (key === '<' || key === '《') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('《', returnReady, frameCount);
+    }
+    if (key === '>' || key === '》') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('》', returnReady, frameCount);
+    }
+    if (key === '!' || key === '！') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('！', returnReady, frameCount);
+    }
+    if (key === '@') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('@', returnReady, frameCount);
+    }
+    if (key === '#') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('#', returnReady, frameCount);
+    }
+    if (key === '$') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('¥', returnReady, frameCount);
+    }
+    if (key === '%') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('%', returnReady, frameCount);
+    }
+    if (key === '^' || key === '……') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('…', returnReady, frameCount);
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('…', returnReady, frameCount);
+    }
+    if (key === '&') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('&', returnReady, frameCount);
+    }
+    if (key === '*') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('*', returnReady, frameCount);
+    }
+    if (key === '(' || key === '（') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('（', returnReady, frameCount);
+    }
+    if (key === ')' || key === '）') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('）', returnReady, frameCount);
+    }
+    if (key === '-') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('-', returnReady, frameCount);
+    }
+    if (key === '_') {
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('—', returnReady, frameCount);
+        lipsArray[lipsArray.length] = new Array(timeSetup);
+        lipsArray[lipsArray.length - 1][0] = new myText('—', returnReady, frameCount);
+    }
+}
+
+
+function keyReleased() {
+    if (key === ' ') {
+        isRecording = false;
+        returnReady = false;
+    }
+
+    if (keyCode === RETURN) {
+        //按下回车键准备换行
+        //returnReady = true;
+        lipsArray[lipsArray.length - 1][0].isReturn = true;
+    }
+
+
 }
