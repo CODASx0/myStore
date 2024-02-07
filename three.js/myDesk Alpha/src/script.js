91626430815c56341e734ad19d7129f35d9eb7c1
CODASx0 <codas0327@gmail.com>
2024-02-07 22:23:02 +0800

    Updated

--- three.js/myDesk Alpha/src/script.js
+++ three.js/myDesk Alpha/src/script.js
@@ -1,6 +1,32 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
+
+/**
+ * Loaders
+ */
+
+
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+gltfLoader.load(
+    '/models/macbookAir13.glb',
+    (gltf) => {
+        gltf.scene.scale.set(0.1, 0.1, 0.1)
+        gltf.scene.rotation.y = Math.PI
+        gltf.scene.position.x = 0
+        gltf.scene.position.y = -0.8
+        scene.add(gltf.scene)
+    }
+)
 
 /**
  * Base
@@ -19,9 +45,21 @@ const scene = new THREE.Scene()
  */
 const testSphere = new THREE.Mesh(
     new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial()
+    new THREE.MeshStandardMaterial()
 )
-scene.add(testSphere)
+//scene.add(testSphere)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3)
+directionalLight.position.set(0.25, 3, -2.25)
+scene.add(directionalLight)
+
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('Light Intensity')
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('Light X')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('Light Y')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('Light Z')
 
 /**
  * Sizes
@@ -31,8 +69,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -50,8 +87,8 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
+const camera = new THREE.PerspectiveCamera(12, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(20, 10, - 20)
 scene.add(camera)
 
 // Controls
@@ -66,12 +103,12 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
 
 /**
  * Animate
  */
-const tick = () =>
-{
+const tick = () => {
     // Update controls
     controls.update()
 
